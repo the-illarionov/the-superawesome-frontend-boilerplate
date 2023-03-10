a57e8de72dc26e73b38c5f12e258ba1ccfb4c2ff
illarionov-company <lavrikanec@gmail.com>
2023-03-10 20:49:05 +0900

    add prettier ignore

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -34,7 +34,7 @@ const config: PlaywrightTestConfig = {
 		/* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
 		actionTimeout: 0,
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://localhost:5173',
+		baseURL: 'https://localhost',
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
@@ -42,7 +42,9 @@ const config: PlaywrightTestConfig = {
 		/* Only on CI systems run the tests headless */
 		headless: true,
 
-		testIdAttribute: 'data-test'
+		testIdAttribute: 'data-test',
+
+		ignoreHTTPSErrors: true
 	},
 
 	/* Configure projects for major browsers */
@@ -93,22 +95,10 @@ const config: PlaywrightTestConfig = {
 		//     channel: 'chrome',
 		//   },
 		// },
-	],
+	]
 
 	/* Folder for test artifacts such as screenshots, videos, traces, etc. */
 	// outputDir: 'test-results/',
-
-	/* Run your local dev server before starting the tests */
-	webServer: {
-		/**
-     * Use the dev server by default for faster feedback loop.
-     * Use the preview server on CI for more realistic testing.
-    Playwright will re-use the local server if there is already a dev-server running.
-     */
-		command: process.env.CI ? 'vite preview --port 5173' : 'vite dev',
-		port: 5173,
-		reuseExistingServer: !process.env.CI
-	}
 }
 
 export default config
