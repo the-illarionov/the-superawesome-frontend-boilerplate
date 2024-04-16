2276ed94fffc549b13f8ad4053f05d7e9313dc20
Alex Illarionov <the.illarionov@gmail.com>
2024-04-16 22:00:29 +0900

    Bump package versions

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  // @ts-expect-error vitest
   test: {
     environment: 'jsdom',
     exclude: [...configDefaults.exclude, 'e2e/*'],
