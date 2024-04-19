ea1357ff36ccc4b560adf552421627b9f5f19e08
Alex Illarionov <the.illarionov@gmail.com>
2024-04-19 11:38:30 +0900

    Add esbuild drop console

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,20 +6,24 @@ import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      vue(),
+      VueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  // @ts-expect-error vitest
-  test: {
-    environment: 'jsdom',
-    exclude: [...configDefaults.exclude, 'e2e/*'],
-    root: fileURLToPath(new URL('./', import.meta.url)),
-  },
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/*'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+    },
+    esbuild: {
+      drop: mode === 'production' ? ['console'] : [],
+    },
+  }
 })
